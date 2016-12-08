754918ec61a78e11d77e72873c90caecdd0ec587
emili <emili@DESKTOP-VU8OA9S>
2016-12-09 01:44:09 +0200

    AddOrderForm edited to support DBSet

--- Travel Agency/AddOrderForm.cs
+++ Travel Agency/AddOrderForm.cs
@@ -87,15 +87,39 @@ namespace Travel_Agency
                         }
                         if (clientsBox.BackColor == Color.LightGreen && offerBox.BackColor == Color.LightGreen && workerComboBox.BackColor == Color.LightGreen && travellersAmountTrackBar.BackColor == Color.LightGreen)
                         {
-                            Offer offer = Program.allOffers[Convert.ToInt32(offerBox.SelectedItem.ToString().Split('.').First())];
-                            Client client = Program.allClients[Convert.ToInt32(clientsBox.SelectedItem.ToString().Split(' ').Last().Remove(clientsBox.SelectedItem.ToString().Split(' ').Last().Length - 1))];
-                            Worker worker = Program.allWorkers[Convert.ToInt32(workerComboBox.SelectedItem.ToString().Split('.').First())];
-                            Order.EmailSend += EmailSendHandler;
-                            //Order order = new Order(offer, client, worker, travellersAmountTrackBar.Value, monthCalendar.SelectionStart, new LogFileWritter(), new ScreenObjectInfoWritter(), emailConfirmationCheckBox.Checked ? new EmailInvoiceSender() : null);
-                            //worker.AssignOrderToWorker(order);
-                            //Program.allOrders.Add(order.OrderNumber, order);
-                            _mainForm.StartThreadQuantityUpdate();
-                            Dispose();
+                            using (var db = new TravelAgencyContext())
+                            {
+                                Worker worker = null;
+                                var workerQuery = from w in db.Workers
+                                                  where w.WorkerNumber == Convert.ToInt32(workerComboBox.SelectedItem.ToString().Split('.').First())
+                                                  select w;
+                                foreach (var item in workerQuery)
+                                {
+                                    worker = item;
+                                }
+                                Offer offer = null;
+                                var offerQuery = from o in db.Offers
+                                                 where o.OfferNumber == Convert.ToInt32(offerBox.SelectedItem.ToString().Split('.').First())
+                                                 select o;
+                                foreach (var item in offerQuery)
+                                {
+                                    offer = item;
+                                }
+                                Client client = null;
+                                var clientQuery = from c in db.Clients
+                                                  where c.ClientNumber == Convert.ToInt32(clientsBox.SelectedItem.ToString().Split(' ').Last().Remove(clientsBox.SelectedItem.ToString().Split(' ').Last().Length - 1))
+                                                  select c;
+                                foreach (var item in clientQuery)
+                                {
+                                    client = item;
+                                }
+                                Order.EmailSend += EmailSendHandler;
+                                Order order = new Order(offer, client, worker, travellersAmountTrackBar.Value, monthCalendar.SelectionStart, new LogFileWritter(), new ScreenObjectInfoWritter(), emailConfirmationCheckBox.Checked ? new EmailInvoiceSender() : null);
+                                worker.AssignOrderToWorker(order);
+                                db.Orders.Add(order);
+                                _mainForm.StartThreadQuantityUpdate();
+                                Dispose();
+                            }
                         }
                     }
                 }
@@ -109,23 +133,32 @@ namespace Travel_Agency
 
         private void ClientsBox_DropDown(object sender, EventArgs e)
         {
-            List<string> list = Program.allClients.Values.Select(i => i.Name + " " + i.LastName + " [Client number: " + i.ClientNumber + "]").ToList();
-            list.Insert(0, "Add new client...");
-            clientsBox.DataSource = new BindingSource(list, null);
+            using (var db = new TravelAgencyContext())
+            {
+                List<string> list = db.Clients.Select(i => i.Name + " " + i.LastName + " [Client number: " + i.ClientNumber + "]").ToList();
+                list.Insert(0, "Add new client...");
+                clientsBox.DataSource = new BindingSource(list, null);
+            }
         }
 
         private void OfferBox_DropDown(object sender, EventArgs e)
         {
-            List<string> list = Program.allOffers.Values.Select(i => i.OfferNumber + ". " +  i.TravelDestination + ", " + i.HotelRanking + ", " + i.Feeding + ", €" + i.Price).ToList();
-            list.Insert(0, "Add new offer...");
-            offerBox.DataSource = new BindingSource(list, null);
+            using (var db = new TravelAgencyContext())
+            {
+                List<string> list = db.Offers.Select(i => i.OfferNumber + ". " + i.TravelDestination + ", " + i.HotelRanking + ", " + i.Feeding + ", €" + i.Price).ToList();
+                list.Insert(0, "Add new offer...");
+                offerBox.DataSource = new BindingSource(list, null);
+            }
         }
 
         private void WorkerComboBox_DropDown(object sender, EventArgs e)
         {
-            List<string> list = Program.allWorkers.Values.Select(i => i.WorkerNumber + ". " + i.Name + " " + i.LastName + ", " + i.Position).ToList();
-            list.Insert(0, "Add new worker...");
-            workerComboBox.DataSource = new BindingSource(list, null);
+            using (var db = new TravelAgencyContext())
+            {
+                List<string> list = db.Workers.Select(i => i.WorkerNumber + ". " + i.Name + " " + i.LastName + ", " + i.Position).ToList();
+                list.Insert(0, "Add new worker...");
+                workerComboBox.DataSource = new BindingSource(list, null);
+            }
         }
 
         private void TravellersAmountTrackBar_ValueChanged(object sender, EventArgs e)
