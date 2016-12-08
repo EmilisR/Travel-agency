ade5c9f5b454119da5f39afeb52c73fa79a16fd6
emili <emili@DESKTOP-VU8OA9S>
2016-12-09 01:53:59 +0200

    Edited ChangeShoftForm and deleted function to change establishment

--- Travel Agency/ChangeShiftForm.cs
+++ Travel Agency/ChangeShiftForm.cs
@@ -35,54 +35,21 @@ namespace Travel_Agency
                 }
                 if (establishmentComboBox.SelectedIndex != -1 && workersBox.SelectedIndex != 1)
                 {
-                    Worker worker = Program.allWorkers[Convert.ToInt32(workersBox.SelectedItem.ToString().Split('.').First())];
-                    string oldPosition = worker.Position;
-                    worker.Position = establishmentComboBox.SelectedItem.ToString();
-                    MessageBox.Show("Old position: " + oldPosition + "\nNew position: " + worker.Position, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    Dispose();
-                }
-            }
-            else
-            {
-                if (establishmentComboBox.SelectedIndex == -1)
-                {
-                    establishmentComboBox.BackColor = Color.Salmon;
-                }
-                else
-                {
-                    establishmentComboBox.BackColor = Color.LightGreen;
-                }
-                if (workersBox.SelectedIndex == -1)
-                {
-                    workersBox.BackColor = Color.Salmon;
-                }
-                else
-                {
-                    workersBox.BackColor = Color.LightGreen;
-                }
-                if (establishmentComboBox.SelectedIndex != -1 && workersBox.SelectedIndex != 1)
-                {
-                    Worker worker = Program.allWorkers[Convert.ToInt32(workersBox.SelectedItem.ToString().Split('.').First())];
-                    double previuosSalary = worker.Salary;
-                    int previuosWorkingHours = worker.WorkingHoursPerWeek;
-                    if (establishmentComboBox.SelectedItem.ToString() == "Full time")
+                    using (var db = new TravelAgencyContext())
                     {
-                        //worker.ChangeEstablishment(1);
-                    }
-                    if (establishmentComboBox.SelectedItem.ToString() == "1/2 time")
-                    {
-                        //worker.ChangeEstablishment(0.5);
-                    }
-                    if (establishmentComboBox.SelectedItem.ToString() == "3/4 time")
-                    {
-                        //worker.ChangeEstablishment(0.75);
-                    }
-                    if (establishmentComboBox.SelectedItem.ToString() == "1/4 time")
-                    {
-                        //worker.ChangeEstablishment(0.25);
-                    }
-                    MessageBox.Show("Previuos salary: €" + previuosSalary + ", new salary: €" + worker.Salary + "\nPreviuos working hours: " + previuosWorkingHours + ", new working hours: " + worker.WorkingHoursPerWeek, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    Dispose();
+                        Worker worker = null;
+                        var query = from w in db.Workers
+                                    where w.WorkerNumber == Convert.ToInt32(workersBox.SelectedItem.ToString().Split('.').First())
+                                    select w;
+                        foreach (var item in query)
+                        {
+                            worker = item;
+                        }
+                        string oldPosition = worker.Position;
+                        worker.Position = establishmentComboBox.SelectedItem.ToString();
+                        MessageBox.Show("Old position: " + oldPosition + Environment.NewLine +  "New position: " + worker.Position, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Dispose();
+                    } 
                 }
             }
         }
