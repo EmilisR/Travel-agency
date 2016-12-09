d758155cbf9e47d4d5d310c75a4a5342e97551be
emili <emili@DESKTOP-VU8OA9S>
2016-12-09 02:17:21 +0200

    Deleted FileInput and moved ReadSetting to Program.cs

--- Travel Agency/Program.cs
+++ Travel Agency/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Data.Entity;
 using System.Linq;
+using System.Configuration;
+using System.IO;
 
 namespace Travel_Agency
 {
@@ -43,7 +45,22 @@ namespace Travel_Agency
                 Console.ReadKey();
             }
         }
-        
-
+        public static string ReadSetting(string key, string filePath)
+        {
+            string result = null;
+            try
+            {
+                ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap();
+                configFileMap.ExeConfigFilename = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())) + @"\" + filePath; // full path to the config file
+                Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
+                AppSettingsSection section = (AppSettingsSection)config.GetSection("appSettings");
+                result = section.Settings[key].Value.ToString() ?? "Not Found";
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+            return result;
+        }
     }
 }
