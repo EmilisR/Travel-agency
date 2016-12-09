b8650e5a6ccb42ca3071ee3780cdff42d374f748
emili <emili@DESKTOP-VU8OA9S>
2016-12-09 15:58:01 +0200

    Edited SendInformationByEmailForm to support DBSet

--- Travel Agency/SendInformationByEmailForm.cs
+++ Travel Agency/SendInformationByEmailForm.cs
@@ -17,48 +17,74 @@ namespace Travel_Agency
 
         private void AboutOrderButton_Click(object sender, EventArgs e)
         {
-            if (Program.allOrders.Count > 0)
+            using (var db = new TravelAgencyContext())
             {
-                List<string> list = Program.allOrders.Values.Select(i => i.OrderNumber + ". " + i.OrderClient.Name + " " + i.OrderClient.LastName + " " + i.TravelOffer.TravelDestination).ToList();
-                ShowObject showObject = new ShowObject(new BindingSource(list, null), typeof(Order));
-                showObject.Text = "Show orders";
-                showObject.showButton.Text = "Send information to E-mail";
-                showObject.showButton.Size = new Size(240, 23);
-                showObject.deleteButton.Visible = false;
-                showObject.ShowDialog();
+                if (db.Orders.Count() > 0)
+                {
+                    List<string> list = db.Orders.Select(i => i.OrderNumber + ". " + i.OrderClient.Name + " " + i.OrderClient.LastName + " " + i.TravelOffer.TravelDestination).ToList();
+                    ShowObject showObject = new ShowObject(new BindingSource(list, null), typeof(Order));
+                    showObject.Text = "Show orders";
+                    showObject.showButton.Text = "Send information to E-mail";
+                    showObject.showButton.Size = new Size(240, 23);
+                    showObject.deleteButton.Visible = false;
+                    showObject.ShowDialog();
+                }
+                else MessageBox.Show("No orders!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else MessageBox.Show("No orders!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         public static void SendEmailOrderHandler(ShowObject sender, EventArgs e)
         {
-            Order order = Program.allOrders[Convert.ToInt32(sender.objectBox.SelectedItem.ToString().Split('.').First())];
-            Task.Run(() => EmailSender.SendIt(order, order.OrderClient.Email, order.OrderRegisterDate, "Order information"));
-            MessageBox.Show("E-mail sent to " + order.OrderClient.Email, "E-mail sent", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            sender.Dispose();
+            using (var db = new TravelAgencyContext())
+            {
+                Order order = null;
+                var ordersQuery = from o in db.Orders
+                            where o.OrderNumber == Convert.ToInt32(sender.objectBox.SelectedItem.ToString().Split('.').First())
+                                  select o;
+                foreach (var item in ordersQuery)
+                {
+                    order = item;
+                }
+                Task.Run(() => EmailSender.SendIt(order, order.OrderClient.Email, order.OrderRegisterDate, "Order information"));
+                MessageBox.Show("E-mail sent to " + order.OrderClient.Email, "E-mail sent", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                sender.Dispose();
+            }
         }
 
         public static void SendEmailClientHandler(ShowObject sender, EventArgs e)
         {
-            Client client = Program.allClients[Convert.ToInt32(sender.objectBox.SelectedItem.ToString().Split(' ').Last().Remove(sender.objectBox.SelectedItem.ToString().Split(' ').Last().Length - 1))];
-            EmailSender.SendIt(client, client.Email, client.RegisterDate, "Client information");
-            MessageBox.Show("E-mail sent to " + client.Email, "E-mail sent", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            sender.Dispose();
+            using (var db = new TravelAgencyContext())
+            {
+                Client client = null;
+                var clientsQuery = from c in db.Clients
+                                  where c.ClientNumber == Convert.ToInt32(sender.objectBox.SelectedItem.ToString().Split(' ').Last().Remove(sender.objectBox.SelectedItem.ToString().Split(' ').Last().Length - 1))
+                                   select c;
+                foreach (var item in clientsQuery)
+                {
+                    client = item;
+                }
+                EmailSender.SendIt(client, client.Email, client.RegisterDate, "Client information");
+                MessageBox.Show("E-mail sent to " + client.Email, "E-mail sent", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                sender.Dispose();
+            }
         }
 
         private void AboutClientButton_Click(object sender, EventArgs e)
         {
-            if (Program.allClients.Count > 0)
+            using (var db = new TravelAgencyContext())
             {
-                List<string> list = Program.allClients.Values.Select(i => i.Name + " " + i.LastName + " [Client number: " + i.ClientNumber + "]").ToList();
-                ShowObject showObject = new ShowObject(new BindingSource(list, null), typeof(Client));
-                showObject.Text = "Show clients";
-                showObject.showButton.Text = "Send information to E-mail";
-                showObject.showButton.Size = new Size(240, 23);
-                showObject.deleteButton.Visible = false;
-                showObject.ShowDialog();
+                if (db.Clients.Count() > 0)
+                {
+                    List<string> list = db.Clients.Select(i => i.Name + " " + i.LastName + " [Client number: " + i.ClientNumber + "]").ToList();
+                    ShowObject showObject = new ShowObject(new BindingSource(list, null), typeof(Client));
+                    showObject.Text = "Show clients";
+                    showObject.showButton.Text = "Send information to E-mail";
+                    showObject.showButton.Size = new Size(240, 23);
+                    showObject.deleteButton.Visible = false;
+                    showObject.ShowDialog();
+                }
+                else MessageBox.Show("No clients!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else MessageBox.Show("No clients!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
